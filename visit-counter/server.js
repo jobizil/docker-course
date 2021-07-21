eb3f26c9e8be2f4fc6aa0edb0ff7f30f6aa59615
Job Ugbem <ugbemjob57@gmail.com>
2021-07-21 10:56:47 +0100

    Docker compose intro

--- visit-counter/server.js
+++ visit-counter/server.js
@@ -11,6 +11,6 @@ app.get('/', (req, res) => {
   })
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.listen(port, () => console.log(`Server running on ${port}`))
